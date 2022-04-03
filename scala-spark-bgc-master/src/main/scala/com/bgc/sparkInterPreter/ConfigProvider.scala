4e38b5a337608a1062700a77fcf3843e60c1fc21
Vikas2kumar <kumar.x.vikas@gmail.com>
2022-04-03 12:23:41 +0100

    Add files via upload
    demo

--- scala-spark-bgc-master/src/main/scala/com/bgc/sparkInterPreter/ConfigProvider.scala
+++
@@ -1,8 +0,0 @@
-package com.bgc.sparkInterPreter
-
-import com.typesafe.scalalogging.LazyLogging
-
-object ConfigProvider extends LazyLogging {
-  lazy val applicationName: String = Option(System.getProperty("spark.com.bgc.analytics.application.name")).getOrElse("AnalyticsName")
-}
-
